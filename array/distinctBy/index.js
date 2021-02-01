935d4609ff4b71b114a60b8225074e66a343339d
Yair <yairopro@gmail.com>
2021-02-02 01:06:53 +0200

    array/distinctBy

---
+++ array/distinctBy/index.js
@@ -0,0 +1,22 @@
+const curry = require('ramda/src/curry');
+
+function distinctBy(areEquals, array) {
+	if (areEquals === undefined)
+		areEquals = Object.is;
+
+	if (array)
+		return array.reduce((result, item, index) => {
+			if (
+				!index
+				|| result.every(picked =>
+					picked !== item
+					&& !areEquals(item, picked)
+				)
+			)
+				result.push(item);
+
+			return result;
+		}, []);
+}
+
+module.exports = curry(distinctBy);
--- array/index.js
+++ array/index.js
@@ -2,3 +2,4 @@ exports.map = require("./map");
 exports.mapColumns = require("./mapColumns");
 exports.join = require("./join");
 exports.match = require("./match");
+exports.distinctBy = require("./distinctBy");
