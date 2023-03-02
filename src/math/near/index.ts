4a2fcb7be8dfb7ae25d2329a90c31d1b7053750b
Yair <yairopro@gmail.com>
2023-03-02 07:26:18 +0200

    near signature changed

--- src/math/near/index.ts
+++ src/math/near/index.ts
@@ -4,7 +4,7 @@ import {curry} from "ramda"
  * @param threshold Included
  * @returns If input is near target by threshold
  */
-function near_core(target: number, threshold: number, input: number){
+function near_core(threshold: number, target: number, input: number){
 	return Math.abs(input - target) <= threshold;
 }
 
